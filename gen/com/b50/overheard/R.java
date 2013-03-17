c931d1f3f608afa7dce45a07f6a86e02613ffcfc
Andrew Glover <aglover@beacon50.com>
2013-03-17 13:56:23 -0400

    minor updates to resource files

--- gen/com/b50/overheard/R.java
+++ gen/com/b50/overheard/R.java
@@ -44,11 +44,9 @@ public final class R {
         public static final int overheard_word=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int next_word=0x7f050003;
-        public static final int take_quiz=0x7f050004;
+        public static final int next_word=0x7f050001;
+        public static final int take_quiz=0x7f050002;
     }
     public static final class style {
         /** 
